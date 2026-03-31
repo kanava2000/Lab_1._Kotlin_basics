b5c875b371d27ee7a16ce28c7b31e782040f0a99
kanava2000 <lizalevchuk20@stud.op.edu.ua>
2026-03-31 21:50:20 +0300

    Initial commit with .gitignore

---
+++ Collections and Extension Functions/Combined Task - Collections and Extensions/src/Task.kt
@@ -0,0 +1,5 @@
+// TODO: Напишіть функцію-розширення List<String>.getImportantWords(): List<String>
+
+fun List<String>.getImportantWords(): List<String> {
+    return this.filter { it.startsWith("K") }.map { it.uppercase() }
+}
